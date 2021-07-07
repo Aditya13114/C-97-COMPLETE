f2f8e3ddff9a3029f3d5972a10b3b9c9e9e3a6c9
Aditya13114 <75022510+Aditya13114@users.noreply.github.com>
2021-07-07 21:00:49 +0530

    Add files via upload

---
+++ guessinGame.py
@@ -0,0 +1,20 @@
+import random
+
+computerguess = random.randint(1,9)
+userguess = int(input("enter the no. between 1 and 9"))
+
+if computerguess == userguess:
+    print("You are correct! Congratulations")
+
+
+if computerguess > userguess:
+    print("Wrong, try again")
+
+   
+if computerguess < userguess: 
+    print("Wrong, try again")
+
+
+    
+
+
